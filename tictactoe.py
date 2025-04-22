dfa2de7a48d662a1b307ff4e01a336f49a0a8cb3
Yousuf Khan <yousuf.khan.email@gmail.com>
2025-04-22 11:15:00 +0100

    Added pretty print of tictactoe board

---
+++ tictactoe.py
@@ -0,0 +1,23 @@
+import random 
+import numpy as np 
+
+def main():
+    ... 
+
+
+#Initialise board 
+def create_board():
+    return np.arange(1, 10).reshape(3,3)
+
+board = create_board()
+
+def print_board(board):
+    board_lines = " " * 5 + "|" + " "*5 + "|" + " "*5  
+    for i in range(np.size(board, 0)):
+        print(board_lines)
+        print(f'  {board[i, 0]}  |  {board[i, 1]}  |  {board[i, 2]}   ')
+        print(board_lines)
+        if i < np.size(board, 0) - 1:
+            print("-"* 18)
+
+print_board(board)
